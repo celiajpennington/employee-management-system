69f35af1cd7014b1e42aeb50e21957d6da441292
Celia Pennington <celiajpennington@gmail.com>
2021-08-25 15:50:57 -0500

    seeded schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,10 +2,10 @@ INSERT INTO department (name)
 VALUES ("Human Resources"), ("R&D"), ("Engineering"), ("Accounting"), ("Sales");
 
 INSERT INTO role (title, salary, department_id)
-VALUE ("manager", 75000.00, 2), ("engineer", 52000, 3), ("accountant", 62500, 4), ("recruiter", 57500, 1), ("sales person", 85650, 5);
+VALUES ("manager", 75000.00, 2), ("engineer", 52000, 3), ("accountant", 62500, 4), ("recruiter", 57500, 1), ("sales person", 85650, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUE ("Dave", "Smith", 1, null), ("Joe", "Green", 1, 1), ("Jackson", "Brown", 3, 2), ("Steven", "Quinn", 5, 2);
+VALUES ("Dave", "Smith", 1, null), ("Joe", "Green", 1, 1), ("Jackson", "Brown", 3, 2), ("Steven", "Quinn", 5, 2);
 
 
 -- INSERT INTO department (name)
